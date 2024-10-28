d276eb0ea0defa791964c4374c3f05a70f292196
Christian_Faria_DEV <112414610+Christian222111@users.noreply.github.com>
2024-10-28 10:11:51 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-let nome = "Christian";;
-console.log("ola mundo");
+let nomeHeroi = "Stark";
+nivelHeroi = "";
+XP = "2500";
+if(XP <= 1000){
+    console.log("O herói de nome "+nomeHeroi + "esta no nivel Ferro");
+}else if(XP >= 1.001 || XP <= 2.000){
+    console.log("O herói de nome "+nomeHeroi + "esta no nivel Bronze");
+}else if(XP >= 2.001 || XP <=5.000 ){
+    console.log("O herói de nome "+nomeHeroi + "esta no nivel Prata");
+}else if(XP >= 6.001 || XP <=7.000){
+    console.log("O herói de nome "+nomeHeroi + "esta no nivel Ouro");
+}else if(XP >= 7.001 || XP <= 8.000){
+    console.log("O herói de nome "+nomeHeroi + "esta no nivel Platina");
+}else if( XP >= 8.001 || XP <= 9.000){
+    console.log("O herói de nome "+nomeHeroi + "esta no nivel Ascendente");
+}else if(XP >= 9.001 || XP <= 10.001){
+    console.log("O herói de nome "+nomeHeroi + "esta no nivel Imortal");
+}else if(XP >= 10.001){
+    console.log("O herói de nome "+nomeHeroi + "esta no nivel Radiantes");
+}
